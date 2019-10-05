53cfe46927af31d2c3f2c28ee8cd8e0e8c6d1a6c
Jonas Eschenburg <jonas@bitwrk.net>
2019-10-05 16:00:58 +0200

    blendinfo.py: refactor, add function for counting ID objects

--- README.md
+++ README.md
@@ -1,90 +1,46 @@
 # blendinfo
-Very small utility to analyze the contents of a .blend file (Blender's internal file format).
+Utility to analyze the contents of a .blend file (Blender's internal file format).
 
-Usage: python3 blendinfo.py somefile.blend
+```
+usage: blendinfo.py [-h] [--info] [--dna] [--id] PATH
 
-The output is a rough dump of the contents of the file which can be further examined using text-based tools.
+Examines .blend files and prints information
+
+positional arguments:
+  PATH        .blend file to examine
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --info      Print information about the .blend file itself
+  --dna       Print detailed information about DNA structs
+  --id        Print summary on 'ID' structs
+```
+The output is empty unless one of the information-selecting arguments is given (--info, --dna etc.).
 
 Example:
 ```
-Header: b'BLENDER-v279'
- -> pointer size: 8
- -> big endian:   False
-Block: b'REND'
- -> size (bytes): 72
- -> SDNA index:   0
- ->      count:   1
- -> old pointer:  140732920679952
-Block: b'TEST'
- -> size (bytes): 65544
- -> SDNA index:   0
- ->      count:   1
- -> old pointer:  4736246280
-Block: b'GLOB'
- -> size (bytes): 1088
- -> SDNA index:   264
- ->      count:   1
- -> old pointer:  140732920679952
- 
- [...]
- 
-Block: b'DNA1'
- -> size (bytes): 90688
- -> SDNA index:   0
- ->      count:   1
- -> old pointer:  4375110432
-Begin DNA1: b'SDNA'
-  4290x b'NAME'
-     0 *next
-     1 *prev
-     2 *data
-     3 *first
-     4 *last
-     5 x
-     6 y
-     7 z
-     8 xmin
-     9 xmax
-     10 ymin
-     11 ymax
-     12 *pointer
-     13 group
-     14 val
-     15 val2
-     16 type
-     17 subtype
-     18 flag
-     19 name[64]
-     20 saved
-     21 data
-     22 len
-     23 totallen
-     24 *newid
-     25 *lib
-     
-  [...]
-  
-  621x b'STRC'
-     0 struct ('Link', 16)
-       0: field *next :: ('Link', 16)
-       1: field *prev :: ('Link', 16)
-       Bytes padding:  0
-     1 struct ('LinkData', 24)
-       0: field *next :: ('LinkData', 24)
-       1: field *prev :: ('LinkData', 24)
-       2: field *data :: ('void', 0)
-       Bytes padding:  0
-     2 struct ('ListBase', 16)
-       0: field *first :: ('void', 0)
-       1: field *last :: ('void', 0)
-       Bytes padding:  0
-     3 struct ('vec2s', 4)
-       0: field x :: ('short', 2)
-       1: field y :: ('short', 2)
-       Bytes padding:  0
-     4 struct ('vec2f', 8)
-       0: field x :: ('float', 4)
-       1: field y :: ('float', 4)
-       Bytes padding:  0
+> blendinfo.py --info barbershop_interior_cpu.blend
+Blend file: Little-endian, 64 bit pointers
 
+> blendinfo.py --id barbershop_interior_cpu.blend
+5722 of 254794 datablocks containing 5722 of 10492836 objects totalling 7432040 of 281459712 bytes are ID
+
+> blendinfo.py --dna barbershop_interior_cpu.blend | head -n 30
+struct Link // 16 bytes [#0]
+    Link       *next                 //    8 bytes, offset 0
+    Link       *prev                 //    8 bytes, offset 8
+struct LinkData // 24 bytes [#1]
+    LinkData   *next                 //    8 bytes, offset 0
+    LinkData   *prev                 //    8 bytes, offset 8
+    void       *data                 //    8 bytes, offset 16
+struct ListBase // 16 bytes [#2]
+    void       *first                //    8 bytes, offset 0
+    void       *last                 //    8 bytes, offset 8
+struct vec2s // 4 bytes [#3]
+    short      x                     //    2 bytes, offset 0
+    short      y                     //    2 bytes, offset 2
+struct vec2f // 8 bytes [#4]
+    float      x                     //    4 bytes, offset 0
+    float      y                     //    4 bytes, offset 4
+[ ... and thousands more ...]
 ```
--- blendinfo.py
+++ blendinfo.py
@@ -1,112 +1,204 @@
 #!/usr/bin/env python3
 
-import sys, struct
+import struct
+import sys
+from typing import List, Tuple, IO
 
-def read_dna1(data):
-	print("Begin DNA1:", data[:4])
-	data = data[4:]
-	
-	names=[]
-	types=[]
-	
-	name, num = struct.unpack("4sI", data[:8])
-	print("  {}x {}".format(num, name))
-	data = data[8:]
-	ofs = 0
-	for i in range(num):
-		slen = data[ofs:].index(b'\000')
-		name = data[ofs:ofs+slen].decode('ascii')
-		names.append(name)
-		ofs += slen + 1
-		print("    ", i,  name)
-	# 4-byte alignment
-	data = data[(ofs + 3) // 4 * 4:]
-		
-	name, num = struct.unpack("4sI", data[:8])
-	print("  {}x {}".format(num, name))
-	data = data[8:]
-	ofs = 0
-	for i in range(num):
-		slen = data[ofs:].index(b'\000')
-		name = data[ofs:ofs+slen].decode('ascii')
-		types.append( (name, 0) )
-		ofs += slen + 1
-	# 4-byte alignment
-	data = data[(ofs + 3) // 4 * 4:]
-	
-	name = struct.unpack("4s", data[:4])
-	print("  {}x {}".format(len(types), name))
-	data = data[4:]
-	ofs = 0
-	for i, (name, _) in enumerate(types):
-		tlen = struct.unpack("H", data[ofs:ofs+2])[0]
-		ofs += 2
-		types[i] = (name, tlen)
-	# 4-byte alignment
-	data = data[(ofs + 3) // 4 * 4:]
-	
-	print("   -> Types:")
-	for i, (name, tlen) in enumerate(types):
-		print("      {}: {} ({} bytes)".format(i, name, tlen))
+TypeInf = Tuple[str, int]
 
-	name, num = struct.unpack("4sI", data[:8])
-	print("  {}x {}".format(num, name))
-	data = data[8:]
-	ofs = 0
-	for i in range(num):
-		typeidx, nfields = struct.unpack("HH", data[ofs:ofs+4])
-		print("    ", i, "struct", types[typeidx])
-		ofs += 4
-		fields_bytes = 0
-		for j in range(nfields):
-			(field_typeidx,field_nameidx) = struct.unpack("HH", data[ofs:ofs+4])
+
+class DNAField:
+	def __init__(self, name: str, offset: int, typeinf: TypeInf, ptr_size: int):
+		self.orig_name = name
+		self.offset = offset
+		self.typeinf = typeinf
+		self.size = typeinf[1]
+		self.dims = []
+		while name.endswith("]"):
+			openbrace = name.index('[')
+			closebrace = name.index(']')
+			dim = int(name[openbrace + 1:closebrace])
+			self.dims.append(dim)
+			self.size *= dim
+			name = name[:openbrace] + name[closebrace + 1:]
+		if name.startswith("*"):
+			self.size = ptr_size
+			self.is_ptr = True
+		else:
+			self.is_ptr = False
+
+	def __str__(self):
+		decl = "{:10} {}".format(self.typeinf[0], self.orig_name)
+		return "{:32} // {:4} bytes, offset {}".format(decl, self.size, self.offset)
+
+
+class DNAStruct:
+	def __init__(self, name, size, fields: List[DNAField]):
+		self.name = name
+		self.size = size
+		self.fields = fields
+		self.is_id = len(fields) > 0 and fields[0].orig_name == "id"
+
+	def __str__(self):
+		return "struct {} // {} bytes{}".format(self.name, self.size, ", is ID" if self.is_id else "")
+
+
+class BlendFile:
+	def __init__(self, blend_file: IO):
+		self.file = blend_file
+		header = self.file.read(12)
+		if not header.startswith(b'BLENDER'):
+			raise ValueError("Not a .blend file (header: {}".format(header))
+		self.PTR_SIZE = 4 if header[7] == b'_' else 8
+		bigend = header[8] == b'V'
+		self.PTR = 'Q' if self.PTR_SIZE == 8 else 'I'
+		self.EF = '>' if bigend else '<'
+		self.HEADER_SIZE = 16 + self.PTR_SIZE
+
+	def __str__(self):
+		return "Blend file: {}, {} bit pointers".format(
+			"Big-endian" if self.EF == '>' else "Little-endian",
+			self.PTR_SIZE * 8
+		)
+
+	def _unpack(self, fmt, data):
+		return struct.unpack(self.EF + fmt, data)
+
+	def _seek_after_header(self):
+		self.file.seek(12, 0)
+
+	def _all_block_headers(self):
+		self._seek_after_header()
+		while True:
+			header = self.file.read(self.HEADER_SIZE)
+			fmt = "4sI" + self.PTR + "II"
+			block, size, oldp, idx, cnt = self._unpack(fmt, header)
+			if block == b'ENDB':
+				break
+			yield block, size, oldp, idx, cnt
+
+	def scan_dna(self) -> List[DNAStruct]:
+		types = None
+		for block, size, oldp, idx, cnt in self._all_block_headers():
+			if block == b'DNA1':
+				data = self.file.read(size)
+				types = self._parse_dna1(data)
+			else:
+				self.file.seek(size, 1)
+		if types is None:
+			raise ValueError("No DNA1 block found")
+		return types
+
+	def count_id_content(self, dna_structs: List[DNAStruct]) -> Tuple[int, int, int, int, int, int]:
+		ndatablocks, ndatablocks_total, nobjs, nobjs_total, nbytes, nbytes_total = 0, 0, 0, 0, 0, 0
+		for block, size, oldp, idx, cnt in self._all_block_headers():
+			self.file.seek(size, 1)
+			ndatablocks_total += 1
+			nobjs_total += cnt
+			nbytes_total += size
+			if idx == 0:
+				continue
+			if idx > len(dna_structs):
+				print("Strange datablock", block, "references DNAStruct", idx)
+				continue
+			dna_struct = dna_structs[idx]
+			if not dna_struct.is_id:
+				continue
+			ndatablocks += 1
+			nobjs += cnt
+			nbytes += cnt * dna_struct.size
+
+		return ndatablocks, ndatablocks_total, nobjs, nobjs_total, nbytes, nbytes_total
+
+	def _parse_dna1(self, data) -> List[DNAStruct]:
+		data = data[4:]
+
+		names = []
+		types = []
+
+		name, num = self._unpack("4sI", data[:8])
+		data = data[8:]
+		ofs = 0
+		for i in range(num):
+			slen = data[ofs:].index(b'\000')
+			name = data[ofs:ofs + slen].decode('ascii')
+			names.append(name)
+			ofs += slen + 1
+		# 4-byte alignment
+		data = data[(ofs + 3) // 4 * 4:]
+
+		name, num = self._unpack("4sI", data[:8])
+		data = data[8:]
+		ofs = 0
+		for i in range(num):
+			slen = data[ofs:].index(b'\000')
+			name = data[ofs:ofs + slen].decode('ascii')
+			types.append((name, 0))
+			ofs += slen + 1
+		# 4-byte alignment
+		data = data[(ofs + 3) // 4 * 4:]
+
+		name = self._unpack("4s", data[:4])
+		data = data[4:]
+		ofs = 0
+		for i, (name, _) in enumerate(types):
+			tlen = self._unpack("H", data[ofs:ofs + 2])[0]
+			ofs += 2
+			types[i] = (name, tlen)
+		# 4-byte alignment
+		data = data[(ofs + 3) // 4 * 4:]
+
+		name, num = self._unpack("4sI", data[:8])
+		data = data[8:]
+		ofs = 0
+		dna_structs = []
+		for i in range(num):
+			typeidx, nfields = self._unpack("HH", data[ofs:ofs + 4])
+
 			ofs += 4
-			field_name = names[field_nameidx]
-			orig_field_name = field_name
-			field_bytes = 8 if field_name.startswith('*') else types[field_typeidx][1]
-			while field_name.endswith(']'):
-				openbrace = field_name.index('[')
-				closebrace = field_name.index(']')
-				array_size = int(field_name[openbrace+1:closebrace])
-				field_bytes *= array_size
-				field_name = field_name[:openbrace] + field_name[closebrace+1:]
-			print("       {:>3} @ {:>5}: field {:16} :: {}".format(j, fields_bytes, orig_field_name, str(types[field_typeidx])))
-			fields_bytes += field_bytes
-		print("       Bytes padding: ", types[typeidx][1] - fields_bytes)
-	# 4-byte alignment
-	data = data[(ofs + 3) // 4 * 4:]
-	
-	print("  Bytes left:", len(data))
-	
-	print("End DNA1")
+			field_ofs = 0
+			dna_fields = []
+			for j in range(nfields):
+				(field_typeidx, field_nameidx) = self._unpack("HH", data[ofs:ofs + 4])
+				field_name = names[field_nameidx]
+				dna_field = DNAField(field_name, field_ofs, types[field_typeidx], self.PTR_SIZE)
+				dna_fields.append(dna_field)  #
+				ofs += 4
+				field_ofs += dna_field.size
+			dna_struct = DNAStruct(types[typeidx][0], types[typeidx][1], dna_fields)
+			dna_structs.append(dna_struct)
+		return dna_structs
 
-file = open(sys.argv[1], mode="rb")
 
-header = file.read(12)
-print("Header:", header)
+def parse_args():
+	import argparse
+	parser = argparse.ArgumentParser(description="Examines .blend files and prints information")
+	parser.add_argument('blendfile', metavar='PATH', help=".blend file to examine", nargs=1)
+	parser.add_argument("--info", action="store_true", help="Print information about the .blend file itself")
+	parser.add_argument("--dna", action="store_true", help="Print detailed information about DNA structs")
+	parser.add_argument("--id", action="store_true", help="Print summary on 'ID' structs")
+	return parser.parse_args()
 
-PTR_SIZE = 4 if header[7] == b'_' else 8
-bigend = header[8] == b'V'
-print(" -> pointer size:", PTR_SIZE)
-print(" -> big endian:  ", bigend)
 
-nblocks = 0
-sizesum = 0
-while True:
-	header = file.read(16 + PTR_SIZE)
-	nblocks += 1
-	print("Block:", header[0:4])
-	size, oldp, idx, cnt = struct.unpack(('>' if bigend else '<') + "xxxxI" + ('Q' if PTR_SIZE == 8 else 'I') + "II", header)
-	sizesum += size
-	print(" -> size (bytes):", size)
-	print(" -> SDNA index:  ", idx)
-	print(" ->      count:  ", cnt)
-	print(" -> old pointer: ", oldp)
-	if header[0:4] == b'ENDB':
-		break
-	data = file.read(size)
-	if header[0:4] == b'DNA1':
-		read_dna1(data)
-	
-print("Blocks read:                       ", nblocks)
-print("Bytes read (incl. 12 bytes header):", sizesum + 12)
+if __name__ == "__main__":
+	try:
+		args = parse_args()
+	except Exception as e:
+		print(e)
+		sys.exit(2)
+
+	for path in args.blendfile:
+		with open(path, mode="r+b") as file:
+			bf = BlendFile(file)
+			if args.info:
+				print(bf)
+			if args.dna or args.id:
+				dna_structs = bf.scan_dna()
+			if args.dna:
+				for i, s in enumerate(dna_structs):
+					print("{} [#{}]".format(s, i))
+					for f in s.fields:
+						print("   ", f)
+			if args.id:
+				print("{} of {} datablocks containing {} of {} objects totalling {} of {} bytes are ID".format(
+					*bf.count_id_content(dna_structs)))
